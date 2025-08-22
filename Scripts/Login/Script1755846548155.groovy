b9eef9061a674e7a8347e8ead1d94732cfb7d602
bowo7 <bowomars@gmail.com>
2025-08-22 14:21:22 +0700

    Commit for first Learn Katalon

--- Scripts/Login/Script1755846548155.groovy
+++ Scripts/Login/Script1755846548155.groovy
@@ -17,3 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+WebUI.closeBrowser()
+
